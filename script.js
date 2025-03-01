3d66640a095e932943bbd2e661d3eb21ce50dbd3
rohitbhoge2001 <ROHITBHOGE12@GMAIL.COM>
2025-03-01 13:39:30 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function startLoveGame() {
+    alert("Let's play a romantic game!");
+}
+function checkAnswer(option) {
+    if (option === 'A') {
+        alert("Correct! ❤️");
+    } else {
+        alert("Try again, my love!");
+    }
+}
